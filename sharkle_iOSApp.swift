846553eeaecca5a60d68fff65464cdafc9450c58
KIMWOOSUNG <40kimwoosung@gmail.co>
2022-02-20 19:39:33 +0900

    initial project create

---
+++ sharkle_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  sharkle_iOSApp.swift
+//  sharkle-iOS
+//
+//  Created by 김우성 on 2022/02/20.
+//
+
+import SwiftUI
+
+@main
+struct sharkle_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
